638533bebc8ce54c21aef4ba4146d88a646c02e0
Aeliu <oqch@163.com>
2017-02-24 14:12:42 +0800

    --del--

--- TPylr.py
+++ TPylr.py
@@ -2,3 +2,11 @@ class studen():
     def __init__(self, name, age):
         self.name = name
         self.age = age
+    def __str__(self):
+        return ("{} is {} years old!".format(self.name, self.age))
+    def __del__(self):
+        print('Oop! I died!')
+
+xming = studen('Xiao ming', 18)
+print(xming)
+del xming
