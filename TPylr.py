86040984b1a4815660de2f4079e2d92fd32fc9b4
Aeliu <oqch@163.com>
2017-02-24 14:27:16 +0800

    --getattr--

--- TPylr.py
+++ TPylr.py
@@ -1,12 +1,19 @@
-class studen():
+class student():
     def __init__(self, name, age):
         self.name = name
         self.age = age
     def __str__(self):
         return ("{} is {} years old!".format(self.name, self.age))
+    __repr__ = __str__
     def __del__(self):
         print('Oop! I died!')
+    def __getattr__(self, item):
+        if item == 'sex':
+            print("{} is a boy!".format(self.name))
+        raise AttributeError('has no this attr')
 
-xming = studen('Xiao ming', 18)
+xming = student('Xiao ming', 18)
 print(xming)
+xming.sex
+xming.diao
 del xming
