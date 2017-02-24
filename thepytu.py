5a8c0d07a3b92249de509623561f6cac0e17f2b3
Aeliu <oqch@163.com>
2017-02-24 10:54:37 +0800

    break continue

--- thepytu.py
+++ thepytu.py
@@ -54,14 +54,35 @@ a, b = 0, 1
 while b < 10:
     print(b)
     a, b = b, a + b
-x = int(input("Please Enter An Integer: "))
-if x < 0:
-    x = 0
-    print('Negative changed to zero')
-elif x == 0:
-    print('Zero')
-elif x == 1:
-    print('Single')
-else:
-    print('More')
+# x = int(input("Please Enter An Integer: "))
+# if x < 0:
+#     x = 0
+#     print('Negative changed to zero')
+# elif x == 0:
+#     print('Zero')
+# elif x == 1:
+#     print('Single')
+# else:
+#     print('More')
+
+words = ['cat', 'window', 'defenestrate']
+for w in words:
+    print(w, len(w))
+for w in words[:]:
+    if len(w) > 6:
+        words.insert(0, w)
+print(words)
+
+for i in range(5):
+    print(i)
+print(range(10))
+print(list(range(10)))
+
+for n in range(2, 100):
+    for x in range(2, n):
+        if n % x == 0:
+            print(n, 'equals', x, '*', n//x)
+            break
+    else:
+        print(n, ' is a prime number.')
 
